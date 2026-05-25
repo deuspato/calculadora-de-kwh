44cb7aa5c78c58d70046f04b229074ae053473a5
deuspato <polarisbranco@proton.me>
2026-05-25 14:04:16 -0300

    First gtk window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use gtk4 as gtk;
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("org.portalit.kwhcalc")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("calculadora de kwh")
+            .build();
+
+        // Show the window.
+        window.present();
+    });
+
+    app.run()
 }
